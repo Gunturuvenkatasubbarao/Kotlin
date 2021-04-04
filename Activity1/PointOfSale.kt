d0cbb9d79993c32a5709e0f1dd752c66c39d5031
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-04-05 01:54:37 +0530

    Update PointOfSale.kt

--- Activity1/PointOfSale.kt
+++ Activity1/PointOfSale.kt
@@ -5,49 +5,70 @@ class PointOfSale
 	
 	constructor(name:String,mob_num:Int,address:String,items:Int)
 	{
-		println("The Details are: ")
-		println("Customer Name : ${name}")
-		println("Customer Mob_Num : ${mob_num}")
-		println("Customer Address: ${address}")
-		println("-------------------------------")
-		println("Item Details")
-		println(" ")
 		
-		for(i in 1..items)
-			{
-				var pro_id = Integer.valueOf(readLine())
-				val pro_name = readLine()!!
-				var pro_price = Integer.valueOf(readLine())
-				var pro_quant = Integer.valueOf(readLine())
+	    
+		println("The Customer Name is : ${name}")
+		println("The Customer Mobile Number is : ${mob_num}")
+		println("The Customer Address is : ${address}")
 				
-				var obj = PointOfSale(pro_id,pro_name,pro_price,pro_quant)
+					
 				
-		
-				
-				
-			}
 	}
-	constructor(pro_id:Int,pro_name:String,pro_price:Int,pro_quant:Int)
-	{
-		println("The Product Id is : ${pro_id}")
-		println("The Product Name is : ${pro_name}")
-		println("The Product Price is : ${pro_price}")
-		println("The Product Quant is : ${pro_quant}")
-		
-		var bill = pro_price*pro_quant
-		println("")
-		println("The Total Bill is : ${bill}")
-	}
+}
+	
 	
    
 	
-}
+
 fun main()
 {
 	var sc = Scanner(System.`in`)
+	println("Customer Name: ")
 	val name = sc.nextLine()
+	println("Customer Mob Num: ")
 	var mob_num:Int = sc.nextInt()
+	println("Customer Address: ")
 	val address = sc.next()
+	println("Enter Number Of Items: ")
 	var items = sc.nextInt()
+	println("Enter Poduct Details:  ")
+	var array = ArrayList<Any>()
+	var bill = 0
+	for(i in 1..items)
+		{
+			println("Enter Product Id: ")
+				var pro_id = Integer.valueOf(readLine())
+		     	array.add(pro_id)
+				println("Enter Product Name: ")
+				val pro_name = readLine()!!
+			    array.add(pro_name)
+				println("Enter Product Price: ")
+				var pro_price = Integer.valueOf(readLine())
+			    array.add(pro_price)
+				println("Enter Product Quantity: ")
+				var pro_quant = Integer.valueOf(readLine())
+			    array.add(pro_quant)
+			    bill = bill+(pro_quant*pro_price)
+		}
 	var obj = PointOfSale(name,mob_num,address,items)
+	println()
+	println("Product Details")
+	var j=0
+	for(i in 1..items)
+		{
+			println("Product Id is : ${array.get(j)}")
+			j++
+			println("product Name is : ${array.get(j)}")
+			j++
+			println("Product Price is : ${array.get(j)}")
+			j++
+			println("Product Quant is : ${array.get(j)}")
+			j++
+			
+		}
+	println()
+	
+	println("The Total bill is ${bill}")
+	println("--------------------")
+	println("Thanks For Shopping!")
 }
